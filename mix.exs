c8584f600ce39f70f1046cfad92a5f639a163a4e
RaymondLoranger <Raymond.Loranger@bell.net>
2020-07-29 09:11:43 -0400

    0.1.13

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Islands.Request.MixProject do
   def project do
     [
       app: :islands_request,
-      version: "0.1.12",
-      elixir: "~> 1.7",
+      version: "0.1.13",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       name: "Islands Request",
       source_url: source_url(),
@@ -27,7 +27,7 @@ defmodule Islands.Request.MixProject do
 
   defp package do
     [
-      files: ["lib", "mix.exs", "README*", "config/persist*.exs"],
+      files: ["lib", "mix.exs", "README*"],
       maintainers: ["Raymond Loranger"],
       licenses: ["MIT"],
       links: %{"GitHub" => source_url()}
@@ -46,14 +46,13 @@ defmodule Islands.Request.MixProject do
     [
       {:mix_tasks,
        github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false},
-      {:persist_config, "~> 0.2", runtime: false},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:islands_config, "~> 0.1", runtime: false},
       {:islands_coord, "~> 0.1"},
       {:islands_island, "~> 0.1"},
       {:islands_player, "~> 0.1"},
-      {:islands_player_id, "~> 0.1"},
-      {:earmark, "~> 1.0", only: :dev},
-      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
-      {:dialyxir, "~> 0.5", only: :dev, runtime: false}
+      {:islands_player_id, "~> 0.1"}
     ]
   end
 end
