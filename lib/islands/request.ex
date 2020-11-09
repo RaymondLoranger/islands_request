b2f25b42a1a0508eb1dec0b1d1b8def342446ea7
RaymondLoranger <Raymond.Loranger@bell.net>
2020-11-09 08:13:06 -0500

    0.1.16

--- lib/islands/request.ex
+++ lib/islands/request.ex
@@ -4,7 +4,8 @@
 defmodule Islands.Request do
   @moduledoc """
   Defines the `request` type for the _Game of Islands_.
-  \n##### #{Islands.Config.get(:book_ref)}
+
+  ##### Based on the book [Functional Web Development](https://pragprog.com/book/lhelph/functional-web-development-with-elixir-otp-and-phoenix) by Lance Halvorsen.
   """
 
   alias Islands.{Coord, Island, Player, PlayerID}
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Request.MixProject do
   def project do
     [
       app: :islands_request,
-      version: "0.1.15",
+      version: "0.1.16",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Islands Request",
@@ -46,7 +46,6 @@ defmodule Islands.Request.MixProject do
     [
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
-      {:islands_config, "~> 0.1", runtime: false},
       {:islands_coord, "~> 0.1"},
       {:islands_island, "~> 0.1"},
       {:islands_player, "~> 0.1"},
