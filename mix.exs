17410b7a84ab8f9b47a5d3c6addeef8c957aa257
RaymondLoranger <Raymond.Loranger@bell.net>
2022-01-03 11:42:57 -0500

    0.1.25

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Request.MixProject do
   def project do
     [
       app: :islands_request,
-      version: "0.1.24",
+      version: "0.1.25",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Islands Request",
